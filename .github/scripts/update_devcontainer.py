89230ff4de6e73676a19d733285036590ab5b1cd
Robert Wilkins III <1147229+genome21@users.noreply.github.com>
2023-05-07 21:09:14 -0500

    Update update_devcontainer.py

--- .github/scripts/update_devcontainer.py
+++ .github/scripts/update_devcontainer.py
@@ -28,8 +28,10 @@ def get_latest_version(extension_id):
 def main():
     """ Main function """
     # Load devcontainer.json
-    with open("/.devcontainer/devcontainer.json", "r") as f:
+    with open("/home/runner/work/roblogic-codespace-template/roblogic-codespace-template/.devcontainer/devcontainer.json", "r") as f:
         devcontainer = json.load(f)
+
+
 
     # Update extensions
     if "extensions" in devcontainer:
@@ -40,7 +42,7 @@ def main():
                 print(f"Updated {extension_id} to version {latest_version}")
 
     # Save updated devcontainer.json
-    with open(".devcontainer/devcontainer.json", "w") as f:
+    with open("/home/runner/work/roblogic-codespace-template/roblogic-codespace-template/.devcontainer/devcontainer.json", "w") as f:
         json.dump(devcontainer, f, indent=4, sort_keys=True)
 
 if __name__ == "__main__":
